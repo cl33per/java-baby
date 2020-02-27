69632a07af0734f7f78dc5612cde36c54270c8bd
cl33per <leeper08@gmail.com>
2020-02-27 15:56:47 -0500

    building file tree

--- Quickstart.java
+++ Quickstart.java
@@ -1,36 +1,9 @@
-// class QuickStart {
-//   public static void main(String[] args) {
-//     int myInt = 9;
-//     double myDouble = myInt; // Automatic casting: int to double
-
-//     System.out.println(myInt);      // Outputs 9
-//     System.out.println(myDouble);   // Outputs 9.0
-//   }
-// }
-
-class Lamp {
-  boolean isOn;
-
-  void turnOn() {
-    isOn = true;
-  }
-
-  void turnOff() {
-   isOn = false;
-  }
-  void displayLightStatus(){
-      System.out.println("Light on? " + isOn);
-  }
-}
-
 class QuickStart {
-public static void main(String[] args) {
-   Lamp l1 = new Lamp(); // create l1 object of Lamp class
-   Lamp l2 = new Lamp(); // create l2 object of Lamp class
-   l1.turnOn();
-   l2.turnOff();
+  public static void main(String[] args) {
+    int myInt = 9;
+    double myDouble = myInt; // Automatic casting: int to double
 
-   l1.displayLightStatus();
-   l2.displayLightStatus();
+    System.out.println(myInt);      // Outputs 9
+    System.out.println(myDouble);   // Outputs 9.0
   }
 }
